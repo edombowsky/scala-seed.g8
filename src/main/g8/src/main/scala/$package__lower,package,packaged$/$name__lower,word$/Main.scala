07dda7690929aefae4762317076f1fa274465bc7
Earl Dombowsky <earl.dombowsky@hitachi-powergrids.com>
2020-11-25 21:57:40 -0800

    make scalastyle proof

--- src/main/g8/src/main/scala/$package__lower,package,packaged$/$name__lower,word$/Main.scala
+++ src/main/g8/src/main/scala/$package__lower,package,packaged$/$name__lower,word$/Main.scala
@@ -2,9 +2,11 @@ package $package;format="lower,package"$
 package $name;format="lower,word"$
 
 object Main extends App {
+  // scalastyle:off println nonascii
   println("─" * 100)
 
   println("hello world")
 
   println("─" * 100)
+  // scalastyle:on println nonascii
 }
